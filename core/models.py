8782fd2361018ff42510f5279a6577976b138ba5
Dipesh <dipeshluitel40@gmail.com>
2026-01-30 19:27:51 +0545

    fix(settings):app added to installed_apps

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class short_urls:
+class short_urls(models.Model):
     orginal_url = models.URLField(max_length=2000)
     short_url = models.CharField(max_length=10,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
