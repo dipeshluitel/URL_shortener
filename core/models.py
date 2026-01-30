fef1d497e5c2e622953c5f0f57296658dc2e8b56
Dipesh <dipeshluitel40@gmail.com>
2026-01-30 18:58:37 +0545

    models created for URL

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class short_urls:
+    orginal_url = models.URLField(max_length=2000)
+    short_url = models.CharField(max_length=10,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    visit = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.orginal_url} --> {self.short_url}"
+    
