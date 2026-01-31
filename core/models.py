f2075ceb4af434cc0bbd5bcaa81cb7e9158e87fe
Dipesh <dipeshluitel40@gmail.com>
2026-01-31 14:37:50 +0545

    fix(models): inherited from User model

--- core/models.py
+++ core/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserCreation(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['user']
+# Create your models here
 class short_urls(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     orginal_url = models.URLField(max_length=2000)
     short_url = models.CharField(max_length=10,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
