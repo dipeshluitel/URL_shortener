0911ded76d0a253bbd48f0f13ac519c7f6b7b36a
Dipesh <dipeshluitel40@gmail.com>
2026-02-01 09:58:23 +0545

    fix(models): unnecessary model calss removerd to remove bug

--- core/models.py
+++ core/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserCreation(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['user']
 # Create your models here
 class short_urls(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
